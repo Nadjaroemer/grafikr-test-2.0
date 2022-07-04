ca4f3cb07f22ed68eafddb5743c473f7ac611de6
Nadjaroemer <nadja.kuti@gmail.com>
2022-07-04 10:46:59 +0200

    changed typo button.jsx to Button.jsx

--- src/views/Landingpage.jsx
+++ src/views/Landingpage.jsx
@@ -1,5 +1,5 @@
 import MainMenu from "../components/MainMenu";
-import Button from "../components/button";
+import Button from "../components/Button";
 import Footer from "../components/Footer";
 
 export default function Landingpage() {
